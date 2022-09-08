2a1b5947a02adb047cc83323947c4132445a543a
Gabriel Echer <gabrielecher14@gmail.com>
2022-09-08 16:16:13 -0300

    Realização ex43

--- exercicios/ex_funcoes/ex43.py
+++ exercicios/ex_funcoes/ex43.py
@@ -1,6 +1,18 @@
 """
 Função que receba um vetor e o preencha com números inteiros aleatórios sem repetição
 """
-"""
-Função que recebe um vetor de inteiros aleatórios e sem repetição
-"""
+import random
+
+
+tamanho = int(input("Digite quantos valores a lista deverá conter: "))
+vetor = []
+
+def preencher_vetor(vetor):
+    while tamanho > len(vetor):
+        vetor = random.sample(range(1, 101), tamanho)
+        return f'{vetor}'
+        
+    
+print(preencher_vetor(vetor))
+
+        
