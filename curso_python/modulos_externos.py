1d6880cc23e60b127c7ce7e89732a168b315ea6e
Gabriel Echer <gabrielecher14@gmail.com>
2022-10-10 16:44:26 -0300

    instalação modulo colorama

---
+++ curso_python/modulos_externos.py
@@ -0,0 +1,18 @@
+"""
+Módulos externos
+
+Utilizamos o pip
+
+- Pacote teste instalado:
+colorama
+
+COLORAMA -> Utilizado para a impressão de textos coloridos no terminal:
+
+from colorama import init
+from colorama import Fore, Back, Style
+
+print(Fore.RED + 'Gabriel')
+print(Back.GREEN + 'TEXTO QUALQUER')
+print(Style.RESET_ALL)
+"""
+
