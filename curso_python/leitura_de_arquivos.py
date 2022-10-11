94621e52034ad5567ddb6e44eedd6b22d0be55f0
Gabriel Echer <gabrielecher14@gmail.com>
2022-10-11 16:03:31 -0300

    inicio manipulação de arquivos

--- curso_python/leitura_de_arquivos.py
+++ curso_python/leitura_de_arquivos.py
@@ -11,10 +11,21 @@ retorna um __io.TextIoWrapper e é com ele que trabalhamos então
 # OBS: Por padrão, a função open() abre o arq para leitura. Este arquivo
 deve existir, se não teremos o erro FileNotFoundError
 
+<_io.TextIOWrapper name='texto.txt' mode='r' encoding='cp1252'>
+
+mode r -> Modo de Leitura. r -> read()
 """
 # Exemplo 
 
-arquivo = open('texto.txt')
+arquivo = open('texto.txt', encoding= 'UTF-8')
 
-print(arquivo)
-print(type(arquivo))
+#print(arquivo)
+#print(type(arquivo))
+
+# Para ler o conteúdo de seu arquivo após a abertura, devemos utilizar a função
+# read()
+
+print(arquivo.read())
+
+# OBS: O python, utiliza o recurso para trabalhar com arquivos chamado cursor.
+# o cursor funciona como o cursor quando estamos escrevendo
