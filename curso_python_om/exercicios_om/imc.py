458d8213cdbf3d5349d375a267ee83824d59346a
Gabriel Echer <gabrielecher14@gmail.com>
2023-04-11 16:00:27 -0300

    começo outro curso

---
+++ curso_python_om/exercicios_om/imc.py
@@ -0,0 +1,11 @@
+# # IMC = peso / (altura * altura)
+
+
+nome = 'Gabriel Echer'
+altura = 1.75
+peso = 73
+
+
+res = peso / altura ** 2
+
+print(f'{nome} tem {altura} de altura\nPesa {peso}kg\nO seu imc é {res}')
