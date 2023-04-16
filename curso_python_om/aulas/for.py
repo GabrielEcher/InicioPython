542475a9052b934f4e1e59910d449656ef8810d5
Gabriel <gabrielecher14@gmail.com>
2023-04-16 10:31:23 -0300

    for e range

---
+++ curso_python_om/aulas/for.py
@@ -0,0 +1,16 @@
+"""
+texto = 'Python'
+novo_texto = ''
+
+for letra in texto:
+    novo_texto += f'*{letra}'
+    print(letra)
+print(novo_texto + '*')
+"""
+
+## range -> range(start, stop, step)
+
+numeros = range(10)
+
+for numero in numeros:
+    print(numero)
