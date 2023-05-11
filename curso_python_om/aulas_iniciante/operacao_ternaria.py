a3c3d594f1afeca8b9d77a3f97497d6959ff5af2
Gabriel Echer <gabrielecher14@gmail.com>
2023-05-11 16:09:12 -0300

    finalização parte inicial curso

---
+++ curso_python_om/aulas_iniciante/operacao_ternaria.py
@@ -0,0 +1,14 @@
+"""
+OPERAÇÃO TERNARIA (condicional em uma linha)
+<valor> if <condicao> else <outro valor>
+"""
+
+#condicao = 10 == 10
+#variavel = 'Valor' if condicao else 'Outro valor'
+#print(variavel)
+
+#digito = 9 # > 9 = 0
+#novo_digito = digito if digito <= 9 else 0
+#print(novo_digito)
+
+print('Valor' if True else 'Outro valor' if True else 'Fim')
